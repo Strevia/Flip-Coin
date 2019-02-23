04b1fbbfc46a866dfe3f2c9be1bfcaa6db888b63
Phantasy146 <32680459+Phantasy146@users.noreply.github.com>
2019-02-22 23:52:47 -0500

    made it so robot tab is clicked when first access page

--- index.js
+++ index.js
@@ -401,4 +401,5 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+document.getElementsByClassName("tablinks")[0].click()
 load();
