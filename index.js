28afdffc86b4e6b08b678092869f73c34488b540
Phantasy146 <32680459+Phantasy146@users.noreply.github.com>
2019-03-01 21:23:03 -0500

    new css

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ UIUpdate = [
   ['res money amount', 'value.res.money.total > 0', '$', false],
   ['things enRobot amount', 'value.things.enRobot.amount > 0', 'Enlightened Robots: ', false],
   ['market selling', 'value.things.artwork.amount > 0', 'Artwork selling for $', false],
-  ['sell art', 'value.things.artwork.amount > 1', 'Sell an Artwork', false],
+  ['sell art', 'value.things.artwork.amount > 0', 'Sell an Artwork', false],
   ['res creat amount', 'value.res.creat.amount > 0', 'Creativity: ', false],
   ['things book amount', 'value.things.book.amount > 0', 'Books: ', false],
   ['things book price', 'value.res.money.total > 0', 'Write Book of Knowledge<br>', false]
